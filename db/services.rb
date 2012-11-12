385819c485a4b6db93c1c5431af004c9fb8718c8
Gerald Bauer <gerald.bauer@gmail.com>
2012-11-12 11:29:58 +0100

    move quotes from sportbook

---
+++ db/services.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+Service.create!( key:'tipp3',      title:'tipp3' )  # tipp3 classic
+Service.create!( key: 'betathome', title: 'bet-at-home' )
+
+## todo: use new version constant for app module e.g. Wettpool::VERSION ??
+Prop.create!( key: 'db.services.version', value: '1' )
