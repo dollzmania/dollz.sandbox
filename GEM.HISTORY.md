1b198794e2e93af2826f1fce8ddc3ebec81b2c35
Gerald Bauer <gerald.bauer@gmail.com>
2012-12-04 09:48:01 +0100

    move gem to its own repo; flatten folders

--- GEM.HISTORY.md
+++
@@ -1,3 +0,0 @@
-### 0.0.1 / 2012-11-13
-
-* Everything is new. First release
--- lib/sportdb/market/version.rb
+++
@@ -1,6 +0,0 @@
-
-module SportDB ; end    # forward reference (for hoe require)
-
-module SportDB::Market
-  VERSION = '0.2.0'
-end
